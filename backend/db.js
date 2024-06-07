74b7f8cdaf7fb703701430ca5da8af8e9c34bfc6
AryanKhilwani <91679675+AryanKhilwani@users.noreply.github.com>
2024-06-08 00:35:48 +0530

    made changes

--- backend/db.js
+++ backend/db.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-mongoose.connect('mongodb+srv://aryankhilwani:<password>@learningmongodb.rgkekbm.mongodb.net/todoApp');
+mongoose.connect('mongodb+srv://aryankhilwani:Aryan%402580@learningmongodb.rgkekbm.mongodb.net/todoApp');
 
 const todo = mongoose.model('Todos', {title: String, description: String, completed: Boolean});
 
