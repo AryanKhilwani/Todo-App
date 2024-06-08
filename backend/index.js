d96935da19e70dec762c2534a148336d0836e940
AryanKhilwani <91679675+AryanKhilwani@users.noreply.github.com>
2024-06-08 23:08:36 +0530

    Made changes

--- backend/index.js
+++ backend/index.js
@@ -1,12 +1,13 @@
 const express = require('express');
 const {createTodo, updateTodo} = require('./types');
 const {todo} = require('./db');
+const cors = require('cors');
 
 const port = 3000;
 const app = express();
 
 app.use(express.json());
-
+app.use(cors());
 
 app.get('/todos',async (req, res) => {
   const getTodos = await todo.find({});
