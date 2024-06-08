cf1d38b34824303e72cc729123f35b3773167483
AryanKhilwani <91679675+AryanKhilwani@users.noreply.github.com>
2024-06-08 23:11:15 +0530

    Frontend

---
+++ frontend/src/components/CreateTodo.jsx
@@ -0,0 +1,7 @@
+export function CreateTodo() {
+    return <div>
+        <input style={{margin: 10, padding: 10}} type= "text" placeholder="Title"></input><br />
+        <input style={{margin: 10, padding: 10}} type= "text" placeholder="Description"></input><br/>
+        <button style={{margin: 10, padding: 10}}>Add todo</button>
+    </div>
+};
